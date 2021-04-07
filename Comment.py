9dd994c9dab9c463d8237f7954fd8800be93b34d
Granzberg <gering225@gmail.com>
2021-04-07 19:55:05 +0300

    Lesson_1_task2_V1.1

--- Comment.py
+++ Comment.py
@@ -6,18 +6,30 @@ class Bookshelf:
         self.genre_book = genre_book
 
     def shelf(self):
-        print(self.author)
-        print(self.name_book)
-        print(self.year_book)
-        print(self.genre_book)
+        print("Автор:       {}".format(self.author))
+        print("Название:    {}".format(self.name_book))
+        print("Год выпуска: {}".format(self.year_book))
+        print("Жанр:        {}".format(self.genre_book))
 
 
 class Comment:
-    def __init__(self, book_comment):
-        self.book_comment = book_comment
+    def __init__(self, first_book_comment, second_book_comment, third_book_comment, fourth_book_comment):
+        self.first_book_comment = first_book_comment
+        self.second_book_comment = second_book_comment
+        self.third_book_comment = third_book_comment
+        self.fourth_book_comment = fourth_book_comment
 
-    def comment_book(self):
-        print("Отзыв: {}".format(self.book_comment))
+    def comment_book1(self):
+        print("Отзыв: {}".format(self.first_book_comment))
+
+    def comment_book2(self):
+        print("Отзыв: {}".format(self.second_book_comment))
+
+    def comment_book3(self):
+        print("Отзыв: {}".format(self.third_book_comment))
+
+    def comment_book4(self):
+        print("Отзыв: {}".format(self.fourth_book_comment))
 
 
 first_book = Bookshelf("Maximilian Maxim", "Maximilian's Adventure", 1999, "adventure")
@@ -25,13 +37,19 @@ second_book = Bookshelf("some author", "some name of book", 2000, "sci-fi")
 third_book = Bookshelf("Emili Bronte", "Wuthering Heights", 1998, "Novel")
 fourth_book = Bookshelf("Erikh Mariya Remark", "Triumphal Arch. Borrowed life", 1988, "Novel")
 
-comment = Comment("Хорошая книга, рекомендую.")
+first_book_comment = Comment("Не читал и не хочу, назване не нравится.")
+second_book_comment = Comment("Странное название для книги.")
+third_book_comment = Comment("Книжка хороша, рекомендую.")
+fourth_book_comment = Comment("Хорошая книга, рекомендую.")
 
-fourth_book.shelf()
+first_book.shelf()
+first_book_comment.comment_book1()
 print("**************************")
 second_book.shelf()
+second_book_comment.comment_book2()
 print("**************************")
 third_book.shelf()
+third_book_comment.comment_book3()
 print("**************************")
 fourth_book.shelf()
-comment.comment_book()
+fourth_book_comment.comment_book4()
