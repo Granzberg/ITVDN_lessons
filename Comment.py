3fa6760a23df6a786d9831bcfda2cd1016a5ed01
Granzberg <gering225@gmail.com>
2021-04-12 10:26:25 +0300

    Lesson_1_task2_V1.5

--- Comment.py
+++ Comment.py
@@ -11,25 +11,31 @@ class Bookshelf:
 
 
 class Comment:
+    first_book_comment = "Не читал и не хочу, назване не нравится."
+    second_book_comment = "Странное название для книги."
+    third_book_comment = "Книжка хороша, рекомендую."
+    fourth_book_comment = "Хорошая книга, рекомендую."
 
-    list_comments = [first_book_comment, second_book_comment, third_book_comment, fourth_book_comment]
-
-    def __init__(self, list_comments):
-        self.list_comments = list_comments
+    def __init__(self, first_book_comment, second_book_comment, third_book_comment, fourth_book_comment):
+        #self.list_comments = list_comments
         self.first_book_comment = first_book_comment
         self.second_book_comment = second_book_comment
         self.third_book_comment = third_book_comment
         self.fourth_book_comment = fourth_book_comment
-        self.list1 = []
+        #self.list1 = []
 
-    def print_list(self):
-        return self.list1.append(self)
+    #def print_list(self):
+     #   return list.append()
 
     def add_list(self):
-        pass
+        first_list_comments = [self.first_book_comment]
+        second_list_comments = [self.second_book_comment]
+        third_list_comments = [self.third_book_comment]
+        fourth_list_comments = [self.fourth_book_comment]
+        return first_list_comments, second_list_comments, third_list_comments, fourth_list_comments
 
     def adding_comments(self):
-        return "{}\nКоментарии:  {}".format(first_book.shelf(), Comment.print_list(self))
+        return "{}\nКоментарии:  {}".format(first_book.shelf(), Comment.add_list(self))
 
 
 first_book = Bookshelf("Maximilian Maxim", "Maximilian's Adventure", 1999, "adventure")
@@ -38,20 +44,20 @@ third_book = Bookshelf("Emili Bronte", "Wuthering Heights", 1998, "Novel")
 fourth_book = Bookshelf("Erikh Mariya Remark", "Triumphal Arch. Borrowed life", 1988, "Novel")
 
 
-first_book_comment = Comment("Не читал и не хочу, назване не нравится.")
-second_book_comment = Comment("Странное название для книги.")
-third_book_comment = Comment("Книжка хороша, рекомендую.")
-fourth_book_comment = Comment("Хорошая книга, рекомендую.")
+#first_book_comment = Comment("Не читал и не хочу, назване не нравится.")
+#second_book_comment = Comment("Странное название для книги.")
+#third_book_comment = Comment("Книжка хороша, рекомендую.")
+#fourth_book_comment = Comment("Хорошая книга, рекомендую.")
 
 
 #print(first_book.shelf())
-print(Comment.adding_comments(first_book_comment))
+print(Comment.adding_comments())
 print("**************************")
 #print(second_book.shelf())
 #print(Comment.adding_comments())
 print("**************************")
 #print(third_book.shelf())
-print(Comment.adding_comments(third_book_comment))
+#print(Comment.adding_comments(third_book_comment))
 print("**************************")
 #print(fourth_book.shelf())
-print(Comment.adding_comments(fourth_book_comment))
+#print(Comment.adding_comments(fourth_book_comment))
