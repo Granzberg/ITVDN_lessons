fd67d48847912fd146ec6f54716a1f2b8f3e680e
Granzberg <gering225@gmail.com>
2021-04-17 16:22:20 +0300

    Lesson_2_task1_V1.0

--- Documets.py
+++ Documets.py
@@ -1,9 +1,6 @@
 class Editor:
-    def __init__(self):
-        pass
-
     def view_document(self):
-        e = open('text.txt', 'x')
+        e = open('text.txt', 'r')
         return e
 
     def edit_document(self):
@@ -11,11 +8,19 @@ class Editor:
 
 
 class ProEditor(Editor):
-    def __init__(self):
-        pass
-
     def edit_document(self):
         print("Редактирование документов разрешено")
 
 
-m = Editor.view_document()
+def passw():
+    key = "123456"
+    if password == key:
+        ProEditor.edit_document()
+    else:
+        Editor.edit_document(e)
+        Editor.view_document(e)
+        
+
+password = input("Введите лицензионный ключ: ")
+e = Editor
+print(e.view_document(e))
