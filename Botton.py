b72e3438cb4821135790d23ee9c0243fe3d3305c
Granzberg <gering225@gmail.com>
2021-04-19 13:25:52 +0300

    Lesson_2_task2_V1.0

---
+++ Botton.py
@@ -0,0 +1,29 @@
+class Figure(object):
+    def __init__(self, side):
+        self.side = side
+
+    def __str__(self):
+        print("Фигура")
+
+
+class Rectangle(Figure):
+    def __init__(self, side02):
+        self.side02 = side02
+
+    def __str__(self):
+        print("Прямоугольник(side, side02)")
+
+
+class Mouse:
+    def __init__(self, click):
+        self.click = click
+
+    def __str__(self):
+        print("Нажатие на кнопку(click)")
+
+
+
+Mouse.__str__(print())
+Rectangle.__str__(print())
+
+
