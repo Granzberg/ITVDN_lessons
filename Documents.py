252de86b9f1a516701dca1d206241b6de8a86c9d
Granzberg <gering225@gmail.com>
2021-04-19 11:54:11 +0300

    Lesson_2_task1_V1.2

--- Documents.py
+++ Documents.py
@@ -1,24 +1,12 @@
 class Editor:
     def view_document(self):
-        edit_my_file = open('text.txt', 'r')
-        print('открытие документа', edit_my_file.name)
-        print('закрытие документа', edit_my_file.closed)
-        edit_my_file.close()
-        print('файл закрыт', edit_my_file.closed)
+        print("Просмотр доступен")
 
     def edit_document(self):
         print("Редактирование документов недоступно для бесплатной версии.")
 
 
 class ProEditor(Editor):
-    def view_document(self):
-        edit_my_file = open('text.txt', 'w')
-        edit_my_file.write('запись в файл!')
-        read_text = edit_my_file.read()
-        print('прочитано')
-        print(read_text)
-        edit_my_file.close()
-
     def edit_document(self):
         print("Редактирование документов разрешено")
 
@@ -31,7 +19,7 @@ def passw(password):
     else:
         Editor.edit_document(self=password)
         Editor.view_document(self=password)
-        
+
 
 password = input("Введите лицензионный ключ: ")
 passw(password)
