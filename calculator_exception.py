9a00f8a7aad12db3e1551bd6382f573458564820
Granzberg <gering225@gmail.com>
2021-04-21 15:41:22 +0300

    Lesson_3_task2_V1.0

---
+++ calculator_exception.py
@@ -0,0 +1,28 @@
+def calculator():
+    while True:
+        try:
+            x = float(input("x = "))
+            choice = input("Выбирите действие(+, -, *, /, **): ")
+            y = float(input("y = "))
+            result = 0
+
+            if choice == "+":
+                result = x + y
+            elif choice == "-":
+                result = x - y
+            elif choice == "*":
+                result = x * y
+            elif choice == "/":
+                result = x / y
+            elif choice == "**":
+                result = x ** y
+        except ZeroDivisionError as e:
+            print("Ошибка: ", e)
+            print("Повторите ввод")
+            print("*" * 30)
+        else:
+            print("Ответ: ", result)
+            break
+
+
+calculator()
