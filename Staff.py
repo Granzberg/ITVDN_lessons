9b892894db353cd80b4c94de3e61917f80a2301f
Granzberg <gering225@gmail.com>
2021-04-22 20:17:05 +0300

    Lesson_3_task3_V1.2

--- Staff.py
+++ Staff.py
@@ -1,54 +1,38 @@
 class Employee(object):
-    def __init__(self, name, surname, year_hiring, staff=[]):
+    def __init__(self, name, surname, year_hiring):
         self.name = name
         self.surname = surname
         self.year_hiring = year_hiring
-        self.staff = staff
 
-    def employee_list(self):
-        for i in range(4):
-            name = input("Имя: ")
-            surname = input("Фамилия: ")
-            year_hiring = input("Год поступления на работу: ")
-            self.staff = "Имя: {}\nФамилия:  {}\nГод поступления на работу:  {}".format(name, surname, year_hiring)
-            if i == 0:
-                self.staff = self.staff00
-                return self.staff00
-            elif i == 1:
-                self.staff = self.staff01
-                return self.staff01
-            elif i == 2:
-                self.staff = self.staff02
-                return self.staff02
-            elif i == 3:
-                self.staff = self.staff03
-                return self.staff03
+    def __str__(self):
+        s = "Имя: {}\nФамилия:  {}\nГод поступления на работу:  {}".format(self.name, self.surname, self.year_hiring)
+        return s
 
-    def my_list(self, s00, s01, s02, s03):
-        s00 = self.staff00
-        s01 = self.staff01
-        s02 = self.staff02
-        s03 = self.staff03
-        print(s00)
-        print(s01)
-        print(s02)
-        print(s03)
 
+def employee_list(x):
     staff00 = []
     staff01 = []
     staff02 = []
     staff03 = []
-    
+    for i in range(4):
+        if i == 0:
+            x = staff00
+            return x
+        elif i == 1:
+            x = staff01
+            return x
+        elif i == 2:
+            x = staff02
+            return x
+        elif i == 3:
+            x = staff03
+            return x
 
-'''    
-e = Employee
-e.employee_list()
 
+name = input("Имя: ")
+surname = input("Фамилия: ")
+year_hiring = input("Год поступления на работу: ")
+staff = Employee(name, surname, year_hiring)
 
-print(Employee.staff00)
-print(Employee.staff01)
-print(Employee.staff02)
-print(Employee.staff03)
-'''
-
+print(employee_list(staff))
 
