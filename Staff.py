21b2bcd2bd536785ed25b583523608da3b1f0de2
Granzberg <gering225@gmail.com>
2021-04-23 13:27:39 +0300

    Lesson_3_task3_V1.5

--- Staff.py
+++ Staff.py
@@ -1,22 +1,27 @@
 class Employee(object):
-    def __init__(self, name, surname, year_hiring, staff):
+    def __init__(self, name, surname, year_hiring):
         self.name = name
         self.surname = surname
         self.year_hiring = year_hiring
-        self.staff = staff
 
     def __str__(self):
-        s = "Имя: {}\nФамилия:  {}\nГод поступления на работу:  {}".format(self.name, self.surname, self.year_hiring)
-        return s
+        return "Имя: {}\nФамилия:  {}\nГод поступления на работу:  {}".format(self.name, self.surname, self.year_hiring)
 
-
+     
 name = "Maxim"
 surname = "Shishov"
 year_hiring = "2020"
-staff = [name, surname, year_hiring]
-
-e = Employee(name, surname, year_hiring, staff)
-print(e)
 
 
+def up_list():
+    staff = []
+    while True:
+        if staff == list:
+            staff.append(e.__str__())
+            print(staff)
+        else:
+            break
 
+
+e = Employee(name, surname, year_hiring)
+up_list()
