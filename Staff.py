fa4f7678843fbb21600dc77427e48105811039e4
Granzberg <gering225@gmail.com>
2021-04-23 10:57:45 +0300

    Lesson_3_task3_V1.3

--- Staff.py
+++ Staff.py
@@ -1,38 +1,29 @@
 class Employee(object):
-    def __init__(self, name, surname, year_hiring):
+    def __init__(self, name, surname, year_hiring, staff):
         self.name = name
         self.surname = surname
         self.year_hiring = year_hiring
+        self.staff = staff
 
     def __str__(self):
         s = "Имя: {}\nФамилия:  {}\nГод поступления на работу:  {}".format(self.name, self.surname, self.year_hiring)
         return s
 
+    def __eq__(self, other):
+        return (self.name, self.surname, self.year_hiring) == (other.name, other.surname, other.year_hiring)
 
-def employee_list(x):
-    staff00 = []
-    staff01 = []
-    staff02 = []
-    staff03 = []
-    for i in range(4):
-        if i == 0:
-            x = staff00
-            return x
-        elif i == 1:
-            x = staff01
-            return x
-        elif i == 2:
-            x = staff02
-            return x
-        elif i == 3:
-            x = staff03
-            return x
+    def up_list(self):
+        Employee.__str__(self.staff(name, surname, year_hiring))
+        return staff
 
 
-name = input("Имя: ")
-surname = input("Фамилия: ")
-year_hiring = input("Год поступления на работу: ")
-staff = Employee(name, surname, year_hiring)
+name = "Maxim"
+surname = "Shishov"
+year_hiring = "2020"
+staff = [name, surname, year_hiring]
 
-print(employee_list(staff))
+e = Employee(name, surname, year_hiring, staff)
+print(e.up_list())
+
+
 
