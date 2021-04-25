f20722cb13b25297ad71b41481afbeca901b57ff
Granzberg <gering225@gmail.com>
2021-04-25 10:37:16 +0300

    Lesson_3_task3_V1.7

--- Staff.py
+++ Staff.py
@@ -8,24 +8,46 @@ class Employee(object):
         return "Имя: {}\nФамилия:  {}\nГод поступления на работу:  {}".format(self.name, self.surname, self.year_hiring)
 
 
-worker = []
-staff = []
-
-while True:
+workers = []
+staff00 = []
+staff01 = []
+staff02 = []
+staff03 = []
+for i in range(4):
     try:
-        b = input("Добавить ещё одного работника(y/n): ")
-
-        if b == "y":
-            name = input("Имя: ")
-            surname = input("Фамилия: ")
-            year_hiring = input("Год постуления: ")
-            e = Employee(name, surname, year_hiring)
-            worker.append(e)
-            staff.append(worker)
-        else:
-            break
+        name = input("Имя: ")
+        surname = input("Фамилия: ")
+        year_hiring = int(input("Год постуления: "))
+        e = Employee(name, surname, year_hiring)
+        if i == 0:
+            staff00.append(e)
+            workers.append(staff00)
+        elif i == 1:
+            staff01.append(e)
+            workers.append(staff01)
+        elif i == 2:
+            staff02.append(e)
+            workers.append(staff02)
+        elif i == 3:
+            staff03.append(e)
+            workers.append(staff03)
 
     except ValueError as error:
         print("Error: ", error)
 
-print(staff)
+
+print("*"*30)
+for personal in staff00:
+    print(personal)
+print("*"*30)
+for personal in staff01:
+    print(personal)
+print("*"*30)
+for personal in staff02:
+    print(personal)
+print("*"*30)
+for personal in staff03:
+    print(personal)
+
+
+
