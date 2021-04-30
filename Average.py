218520fc10e5dc3cda45a1d67a11b118415da981
Granzberg <gering225@gmail.com>
2021-04-30 11:38:04 +0300

    Lesson_5_task1_V1.1

--- Average.py
+++ Average.py
@@ -1,16 +1,24 @@
 args = int(input("Введите числовые аргументы: "))
 args = map(int, str(args))
 args = list(args)
+print(args)
 
 
-def my_average(args):
+def my_average(*numb):
     the_sum = 0
-    for _ in args:
+    for _ in numb:
         the_sum = the_sum + _
 
-    x = len(args)
+    x = len(numb)
     the_sum = the_sum / x
     return the_sum
 
 
+def arg(*numb):
+    return sum(numb)/len(numb)
+
+
 print(my_average(args))
+
+print(arg(args))
+print(arg(*range(15)))
