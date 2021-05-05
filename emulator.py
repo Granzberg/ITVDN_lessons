9dd56d2e7ee2eed75bc4a9130e2dc3168f21b01b
Granzberg <gering225@gmail.com>
2021-05-05 17:35:57 +0300

    upgrade program

--- emulator.py
+++ emulator.py
@@ -1,29 +1,29 @@
-emulator = {}
-
-
-def my_emulator():
+def adding_to_dict():
     my_url = input("Ссылка которую нужно сократить: ")
     my_new_name = input("Новое имя ссылки: ")
     new_values = {my_new_name: my_url}
     emulator.update(new_values)
 
 
-my_emulator()
-link_request = input("Хотите получить свою ссылку(y/n): ")
-
-
-def branching(link_request):
+def get_linc_from_dict():
     while True:
+        link_request = input("Хотите получить свою ссылку(y/n): ")
         if link_request == "y":
             print(emulator.get(input("Имя: ")))
-            link_request = input("Хотите получить свою ссылку(y/n): ")
         elif link_request == "n":
-            new_link_request = input("Хотите добавить еще ссылку?(y/n/e): ")
-
-            if new_link_request == "y":
-                my_emulator()
-            elif new_link_request == "n":
-                break
+            break
 
 
-branching(link_request)
+def logic():
+    adding_to_dict()
+    while True:
+        new_link_request = input("Хотите добавить еще ссылку?(y/n/e): ")
+        if new_link_request == "y":
+            adding_to_dict()
+        elif new_link_request == "n":
+            get_linc_from_dict()
+            break
+
+
+emulator = {}
+logic()
