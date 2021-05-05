55b6af2fe8484319488ab444ffb19584ba0aaa22
Granzberg <gering225@gmail.com>
2021-05-05 11:46:16 +0300

    Lesson_6_task2_V1.0

---
+++ emulator.py
@@ -0,0 +1,5 @@
+my_url = 'https://www.youtube.com/watch?v=PsBC1Db2GYQ&ab_channel=PetrentoGames'
+
+emulator = {"https://www.youtube.com/PetrentoGames": my_url}
+
+print(emulator.get('https://www.youtube.com/PetrentoGames'))
