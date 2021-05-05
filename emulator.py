677ab72dda710cd3495eb81562e1f7541bb20276
Granzberg <gering225@gmail.com>
2021-05-05 13:41:53 +0300

    Lesson_6_task2_V1.2

--- emulator.py
+++ emulator.py
@@ -1,12 +1,29 @@
-my_url = input("Ссылка которую нужно сократить: ")
-my_new_name = input("Новое имя ссылки: ")
+emulator = {}
 
-emulator = {my_new_name: my_url}
 
+def my_emulator():
+    my_url = input("Ссылка которую нужно сократить: ")
+    my_new_name = input("Новое имя ссылки: ")
+    new_values = {my_new_name: my_url}
+    emulator.update(new_values)
+
+
+my_emulator()
 link_request = input("Хотите получить свою ссылку(y/n): ")
 
-if link_request == "y":
-    print(emulator.get(my_new_name))
-else:
-    print("Спасибо. До свидания")
 
+def branching(link_request):
+    while True:
+        if link_request == "y":
+            print(emulator.get(input("Имя: ")))
+            link_request = input("Хотите получить свою ссылку(y/n): ")
+        elif link_request == "n":
+            new_link_request = input("Хотите добавить еще ссылку?(y/n/e): ")
+
+            if new_link_request == "y":
+                my_emulator()
+            elif new_link_request == "n":
+                break
+
+
+branching(link_request)
