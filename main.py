6c24c58fb97fccad7c0b15122c7c3c2e80d7bee8
Granzberg <gering225@gmail.com>
2021-05-07 17:10:46 +0300

    Lesson_7_additional_taskV1.1

---
+++ main.py
@@ -0,0 +1,5 @@
+#import my_modul
+from my_modul import mod
+
+#print(my_modul.mod())
+print(mod())
--- my_modul.py
+++ my_modul.py
@@ -1,18 +1,13 @@
-'''
+import math
 n = int(input("n = "))
-lst = [2]
+lst = []
 
-for num in range(2, n + 1):
-    for i in range(2, num):
-        if num % i == 0:
-            break
-        else:
+
+def mod():
+    for num in range(2, n + 1):
+        if all(num % i != 0 for i in range(2, int(math.sqrt(num))+1)):
             lst.append(num)
-            break
+    return lst
 
-print(lst)
-'''
 
-for num in range(2, 101):
-    if all(num % i != 0 for i in range(2, num)):
-       print(num)
+
