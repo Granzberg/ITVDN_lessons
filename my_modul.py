7f8a4ba3f5782da809c03eba33dcc59ed30b68f5
Granzberg <gering225@gmail.com>
2021-05-07 14:04:20 +0300

    Lesson_7_additional_task_V1.0

---
+++ my_modul.py
@@ -0,0 +1,18 @@
+'''
+n = int(input("n = "))
+lst = [2]
+
+for num in range(2, n + 1):
+    for i in range(2, num):
+        if num % i != 0:
+            break
+        else:
+            lst.append(num)
+            break
+
+print(lst)
+'''
+
+for num in range(2, 101):
+    if all(num % i != 0 for i in range(2, num)):
+       print(num)
