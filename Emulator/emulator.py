036d48c4a1efb07f1e3bba98df72bdd69c6802b3
Granzberg <gering225@gmail.com>
2021-05-11 20:19:34 +0300

    logic false

--- Emulator/emulator.py
+++ Emulator/emulator.py
@@ -1,42 +1,45 @@
-def adding_to_dict():
-    # Функция которая создает ключ/значение и проверяет его что бы не было повторений.
-    try:
+#import start_emulator
+
+
+class Emulator(object):
+    def __init__(self, my_url, my_new_name, link_request, new_link_request, emulator={}):
+        self.my_url = my_url
+        self.my_new_name = my_new_name
+        self.link_request = link_request
+        self.new_link_request = new_link_request
+        self.emulator = emulator
+
+    def adding_to_dict(self, my_url, my_new_name):
+        # Функция которая создает ключ/значение и проверяет его что бы не было повторений.
+        try:
+            while True:
+                new_values = {my_new_name: my_url}
+        # Проверка существует ли имя.
+                check = self.emulator.get(my_new_name, 0)
+                if check != 0:
+                    raise KeyError
+                else:
+                    self.emulator.update(new_values)
+                    break
+        except KeyError as error:
+            print("Такой ключ существует\nСоздайте другой ключ!", error)
+
+    def get_linc_from_dict(self, link_request):
+        # Функия на выдачи сылки по ключу. Не импортирывать!.
         while True:
-            my_url = input("Ссылка которую нужно сократить: ")
-            my_new_name = input("Новое имя ссылки: ")
-            new_values = {my_new_name: my_url}
-    # Проверка существует ли имя.
-            check = emulator.get(my_new_name, 0)
-            if check != 0:
-                raise KeyError
-            else:
-                emulator.update(new_values)
+            #start_emulator.loop2()
+            if link_request == "y":
+                print(self.emulator.get(input("Имя: ")))
+            elif link_request == "n":
                 break
-    except KeyError as error:
-        print("Такой ключ существует\nСоздайте другой ключ!", error)
 
-
-def get_linc_from_dict():
-    # Функия на выдачи сылки по ключу. Не импортирывать!.
-    while True:
-        link_request = input("Хотите получить свою ссылку(y/n): ")
-        if link_request == "y":
-            print(emulator.get(input("Имя: ")))
-        elif link_request == "n":
-            break
-
-
-def logic():
-    # Функция выбора действия. Не импортировать!
-    adding_to_dict()
-    while True:
-        new_link_request = input("Хотите добавить еще ссылку?(y/n/e): ")
-        if new_link_request == "y":
-            adding_to_dict()
-        elif new_link_request == "n":
-            get_linc_from_dict()
-            break
-
-
-emulator = {}
-
+    def logic(self, my_url, my_new_name, new_link_request, link_request):
+        # Функция выбора действия. Не импортировать!
+        self.adding_to_dict(my_url, my_new_name)
+        while True:
+            #start_emulator.loop()
+            if new_link_request == "y":
+                self.adding_to_dict(my_url, my_new_name)
+            elif new_link_request == "n":
+                self.get_linc_from_dict(link_request)
+                break
--- Emulator/start_emulator.py
+++ Emulator/start_emulator.py
@@ -1,3 +1,21 @@
-from emulator import logic
+import emulator
 
-logic()
+
+my_url = input("Ссылка которую нужно сократить: ")
+my_new_name = input("Новое имя ссылки: ")
+#my_url = 'sdsdsdsdsd'
+#my_new_name = 'da'
+
+print(emulator.Emulator.logic(my_url, my_new_name))
+print(emulator.Emulator.adding_to_dict(my_url, my_new_name))
+
+
+def loop():
+    new_link_request = input("Хотите добавить еще ссылку?(y/n/e): ")
+    print(emulator.Emulator.logic(new_link_request))
+
+def loop2():
+    link_request = input("Хотите получить свою ссылку(y/n): ")
+    print(emulator.Emulator.get_linc_from_dict(link_request))
+
+
