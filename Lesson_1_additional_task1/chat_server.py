e4581d2e9b24ac7430c0bb5fa59eaace10544a22
Granzberg <gering225@gmail.com>
2021-05-19 14:35:47 +0300

    'test'

---
+++ Lesson_1_additional_task1/chat_server.py
@@ -0,0 +1,17 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', 8000))
+client = []
+print('Start')
+while True:
+    data, addres = sock.recvfrom(1024)
+    print(addres[0], addres[1])
+    if addres not in client:
+        client.append(addres)
+
+    for clients in client:
+        if clients == addres:
+            continue
+
+        sock.sendto(data, clients)
