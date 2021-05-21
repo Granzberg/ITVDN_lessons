50f61fd242c765bc0272677bd1b6af50bce21d43
Granzberg <74976017+Granzberg@users.noreply.github.com>
2021-05-21 18:15:03 +0300

    Update chat_client.py

--- Lesson_1_additional_task1/chat_client.py
+++ Lesson_1_additional_task1/chat_client.py
@@ -6,5 +6,5 @@ port = 8000
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((host, port))
 message = input('Сообщение: ')
-sock.send(message.encode('utf-8'))
+sock.send(bytes(message.encode('utf-8')))
 sock.close()
