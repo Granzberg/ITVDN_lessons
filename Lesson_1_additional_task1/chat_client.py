379ac420bbaf93f21d9e50ee3c0a8454f173ba35
Granzberg <gering225@gmail.com>
2021-05-21 18:00:18 +0300

    Lesson_1_additional_task_1_V1.5

--- Lesson_1_additional_task1/chat_client.py
+++ Lesson_1_additional_task1/chat_client.py
@@ -1,22 +1,10 @@
 import socket
 
-host = socket.gethostbyname(socket.gethostname())
+host = 'localhost'
 port = 8000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((host, port))
-
-name = input('Введите псевдоним: ')
-
-
-while True:
-    try:
-        message = input('Text: ')
-        sock.send((name + ': ' + message).encode('utf-8'))
-        data = sock.recv(1024)
-        data = data.decode('utf-8')
-        print(data)
-    except KeyboardInterrupt:
-        sock.close()
-        print('Stop')
-        break
+message = input('Сообщение: ')
+sock.send(message.encode('utf-8'))
+sock.close()
--- Lesson_1_additional_task1/chat_server.py
+++ Lesson_1_additional_task1/chat_server.py
@@ -1,35 +1,18 @@
 import socket
 
-
-host = socket.gethostbyname(socket.gethostname())
+host = 'localhost'
 port = 8000
-
-clients = []
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host, port))
 sock.listen(5)
 
-print('Start')
-
-client, addr = sock.accept()
-
 while True:
     try:
-        print(addr[0], addr[1])
-        if addr not in clients:
-            clients.append(addr)
-
+        client, addr = sock.accept()
     except KeyboardInterrupt:
         sock.close()
-        print('Stop')
         break
-
     else:
-        result = client.recv(1024)
-        print(result.decode('utf-8'))
-        for client in clients:
-            sock.sendto(result, client)
-            print('Sanded.')
-
-sock.close()
+        message = sock.recv(1024).strip()
+        print(message.decode('utf-8'))
+        client.close()
