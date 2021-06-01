e96fa83500531aa91a68d2ad9c932c4ec2946da1
Granzberg <gering225@gmail.com>
2021-06-01 13:14:51 +0300

    Lesson_3_additional_task1_V1.0_do not work

--- Lesson_3_additional_task1/TCP_socket_client.py
+++ Lesson_3_additional_task1/TCP_socket_client.py
@@ -1,9 +1,11 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('', 8000))
+sock.connect(('localhost', 8000))
 while True:
-    message = input('Сообщение: ')
+    a = input('a =  ')
+    b = input('b =  ')
+    message = a + ' ' + b
     sock.send(message.encode('utf-8'))
     data = str(sock.recv(1024), 'utf-8')
 
--- Lesson_3_additional_task1/TCP_socket_server.py
+++ Lesson_3_additional_task1/TCP_socket_server.py
@@ -1,15 +1,41 @@
+import threading
+import time
 import socketserver
 
 
 class MyTCPServer(socketserver.BaseRequestHandler):
+
     def handle(self):
         self.data = str(self.request.recv(1024), 'utf-8')
         print("{}:{} wrote:".format(self.client_address[0], self.client_address[1]))
         print("Message: " + self.data)
-        self.data = int(self.data[0]) + int(self.data[-1])
-        self.data = str(self.data)
+        trigger.set()
+        trigger.clear()
 
-        self.request.sendall(self.data.encode('utf-8'))
+    def logic_calc(self, data):
+        trigger.wait()
+        data = int(data[0]) + int(data[-1])
+        self.logic_shutdown(data)
+        self.request.sendall(str(self.data).encode('utf-8'))
+
+    def logic_shutdown(self, data):
+        if data == 'close':
+            socketserver.TCPServer.server_close()
+
+    def separation(self):
+
+        task1 = threading.Thread(target=self.handle)
+        task2 = threading.Thread(target=self.logic_calc)
+        started_at = time.time()
+
+        task1.start()
+        task2.start()
+
+        task1.join()
+        task2.join()
+
+
+trigger = threading.Event()
 
 
 if __name__ == "__main__":
