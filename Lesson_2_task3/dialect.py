14baba36b35f79404bfa4cb5e219184f6c07aa1d
Granzberg <gering225@gmail.com>
2021-06-05 18:10:15 +0300

    Lesson_2_task3_V1.0

---
+++ Lesson_2_task3/dialect.py
@@ -0,0 +1,36 @@
+import csv
+
+
+class CustomDialect(csv.Dialect):
+    quoting = csv.QUOTE_ALL
+    quotechar = "*"
+    delimiter = "!"
+    lineterminator = '\n'
+
+
+class CustomDialect1(csv.Dialect):
+    quoting = csv.QUOTE_ALL
+    quotechar = "~"
+    delimiter = "$"
+    lineterminator = '\n'
+
+
+csv.register_dialect('tester', CustomDialect)
+csv.register_dialect('tester1', CustomDialect1)
+
+
+with open('data.csv', 'w+') as file:
+    #writer = csv.writer(file, dialect='tester')
+    writer = csv.writer(file, dialect='tester1')
+    writer.writerow(["1", "9000", "lasdadasj", "ldkadsadsa"])
+    writer.writerow(["1", "9000", "lasdadasj", "ldkadsadsa"])
+    writer.writerow(["1", "9000", "lasdadasj", "ldkadsadsa"])
+    writer.writerow(["1", "9000", "lasdadasj", "ldkadsadsa"])
+    writer.writerow(["1", "9000", "lasdadasj", "ldkadsadsa"])
+with open('data.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        print(row)
+    file.close()
+
+
