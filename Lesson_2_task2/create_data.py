918c02ccfd68198a8d23d8f1fc43a4a59501bcc8
Granzberg <gering225@gmail.com>
2021-06-08 17:01:40 +0300

    update

--- Lesson_2_task2/create_data.py
+++ Lesson_2_task2/create_data.py
@@ -2,8 +2,6 @@ import xml.etree.ElementTree as ET
 
 tree = ET.parse('data.xml')
 root = tree.getroot()
-#workers = list(tree.iter())  # root.getchildren() - эта конструкция устарела...
-
 
 for person_data in root:
     print('PK: ', (person_data.attrib, person_data.get('pk')))
@@ -11,18 +9,11 @@ for person_data in root:
     element2 = getattr(person_data.find('./last_name'), 'text', None)
     element3 = getattr(person_data.find('./age'), 'text', None)
     print('{} {} {}'.format(element1, element2, element3))
-'''student_data.find('./first_name').text,
-    student_data.find('./last_name').text,
-    student_data.find('./age').text
-    не работате пишет ошибку ('nonetype' object has no attribute 'text) '''
 
 first_names = root.findall('./person/first_name')
 last_names = root.findall('./person/last_name')
 ages = root.findall('./person/age')
 
-#element1 = getattr(person_data.find('./first_name'), 'text', None)
-#element2 = getattr(person_data.find('./last_name'), 'text', None)
-#element3 = getattr(person_data.find('./age'), 'text', None)
 print('*'*75)
 for values in zip(first_names, last_names, ages):
     row = {value.tag: value.text for value in values}
