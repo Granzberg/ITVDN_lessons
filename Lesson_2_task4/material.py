31e44155e22e1c8e7ca62cb4f1b638dbc4ab30fd
Granzberg <gering225@gmail.com>
2021-06-16 13:08:49 +0300

    'up'

--- Lesson_2_task4/material.py
+++ Lesson_2_task4/material.py
@@ -1,15 +1,15 @@
 import sqlite3
 
 conn = sqlite3.connect(':memory:')
-c =conn.cursor()
-weight = 25#input('weight: ')
-height = 50#input('height: ')
-c.execute('''
-    INSERT INTO stocks
-    ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-    weight, 
-    height, 
-    material_characteristics)
-    VALUES(?, ?, ?, ?), data
-''')
+weight = 25 #input('weight: ')
+height = 50 #input('height: ')
 data = (1, weight, height, 0)
+conn.execute('CREATE TABLE "stocks"("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "weight", "height", "material_characteristics")')
+conn.execute('SELECT * FROM "stocks"')
+conn.execute('INSERT INTO "stocks"("id", weight, height, material_characteristics) '
+             'VALUES(1, "weight", "height", 0)')
+
+material =conn.execute('SELECT * FROM "stocks"').fetchall()
+print(material)
+cursor = conn.execute('SELECT * FROM "stocks";')
+print(cursor.fetchone())
